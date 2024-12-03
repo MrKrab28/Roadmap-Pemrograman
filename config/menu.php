5d353cc9336571b668649d2898267416baf8faaa
MrKrab28 <mr.krab2810@gmail.com>
2024-12-04 01:01:11 +0800

    first commit

---
+++ config/menu.php
@@ -0,0 +1,11 @@
+<?php
+return
+[
+    [
+        'route-active' => 'dashboard',
+        'label' => 'Dashboard',
+        'icon' => 'bx-home-circle',
+        'route-name' => 'dashboard',
+        'admin-only' => false,
+    ],
+];
