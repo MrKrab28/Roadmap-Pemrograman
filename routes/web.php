a134b09f66d71d449b0412ae9cc998930109c213
MrKrab28 <mr.krab2810@gmail.com>
2024-12-21 20:00:28 +0800

    make Auth Route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['guest:user,admin'])->group(function () {
+
+
+    Route::get('/', [AuthController::class, 'login'])->name('login');
+    Route::get('/register', [AuthController::class, 'regis'])->name('register');
+    Route::post('/auth', [AuthController::class, 'authenticate'])->name('authenticate');
+    Route::post('/register', [AuthController::class, 'registerUser'])->name('registerUser');
 });
