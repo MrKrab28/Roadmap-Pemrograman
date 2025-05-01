df4cee0f3a41d0477894af44a9a846ba8de4a1d9
MrKrab28 <mr.krab2810@gmail.com>
2025-05-01 19:10:39 +0800

    fix connection seeder

--- database/seeders/NodeSeeder.php
+++ database/seeders/NodeSeeder.php
@@ -177,7 +177,20 @@ class NodeSeeder extends Seeder
 
                 ['kode' => 'lvl4-1', 'label' => 'Pengenalan Blade Templating Engine', 'roadmap_id' => 3],
                 ['kode' => 'lvl4-2', 'label' => 'Basic Syntax Structur', 'roadmap_id' => 3],
-                ['kode' => 'lvl4-3', 'label' => 'Basic Layout', 'roadmap_id' => 3],
+                ['kode' => 'lvl4-3', 'label' => 'Model & Database', 'roadmap_id' => 3],
+
+
+
+
+
+                // ['kode' => 'lvl5', 'label' => 'Blade Direktif', 'roadmap_id' => 3],
+                // ['kode' => 'lvl5-1', 'label' => 'Direktif Blade (@if, @foreach, @for)', 'roadmap_id' => 3],
+                // ['kode' => 'lvl5-2', 'label' => 'Data Dinamis Dalam Tampilan Blade ', 'roadmap_id' => 3],
+                // ['kode' => 'lvl5-3', 'label' => 'Blade Form & Laravel Form Helper', 'roadmap_id' => 3],
+                // ['kode' => 'lvl5-4', 'label' => '@include', 'roadmap_id' => 3],
+
+
+
 
 
                 ]
